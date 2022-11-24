7db2d7cc2aba7fe581d6bec5e22c2d32d27f5d49
Adrien TACHER <tacheradrien@gmail.com>
2022-11-24 11:44:48 +0100

    modification index

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <title>Cours PHP / MySQL</title>
+        <title>MySQL</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="cours.css">
+        <link rel="stylesheet" href="style_index.css">
     </head>
     <body>
-        <h1>Bases de données MySQL</h1>
         <?php
             try
             {
                 // On se connecte à MySQL
-                $mysqlClient = new PDO('mysql:host=localhost;dbname=sae;charset=utf8', 'root', '');
+                $host = 'mysql:host=localhost;dbname=sae;charset=utf8';
+                $user = 'root';
+                $passeworld = '';
+                $mysqlClient = new PDO($host, $user, $passeworld);
             }
             catch(Exception $e)
             {
@@ -22,17 +24,28 @@
             // Si tout va bien, on peut continuer
 
             // On récupère tout le contenu de la table recipes
-            $sqlQuery = 'SELECT * FROM oeuvre';
+            $sqlQuery = 'SELECT * FROM films';
             $recipesStatement = $mysqlClient->prepare($sqlQuery);
             $recipesStatement->execute();
             $recipes = $recipesStatement->fetchAll();
-
-            // On affiche chaque recette une à une
-            foreach ($recipes as $recipe) {
-                echo '<p>'.$recipe['titre'].'</p>';
-                echo '<img src="'.$recipe['photo'].'">';
-                echo "<br>";
-            }
         ?>
+            <section>
+            <h1>film</h1>
+            <ul class="carousel">
+                <?php
+                //On affiche chaque recette une à une
+                foreach ($recipes as $recipe) {
+                    echo '<li>
+                            <h2>'.$recipe["title"].'</h2>
+                            <a href="oeuvre.php"><img src='.$recipe["urlImg"].' class="agrandir_oeuvre"></a>
+                        </li>';
+
+                    /*echo '<p>'.$recipe['title'].'</p>';
+                    echo '<img src="'.$recipe['urlImg'].'">';
+                    echo "<br>";*/
+                }
+                ?>
+            </ul>
+        </section>
     </body>
 </html>
