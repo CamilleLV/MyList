a1d79fc76e75f0132adfe98751dcd24d2e6d9600
BROUTIER CHARLENE p2100510 <charlene.broutier@etu.univ-lyon1.fr>
2022-12-27 14:58:22 +0000

    Correction de bug dans le script

--- HTML et CSS/Assets/JavaScript/script_suggerer_oeuvre.js
+++ HTML et CSS/Assets/JavaScript/script_suggerer_oeuvre.js
@@ -1,52 +1,84 @@
-console.log("JS");
+const film = document.getElementById('film');
+const serie = document.getElementById('serie');
+const livre = document.getElementById('livre');
+const duree = document.getElementById('duree');
+const duree_label = document.getElementById('duree_label');
+const nb_ep = document.getElementById('nb_ep');
+const nb_ep_label = document.getElementById('nb_ep_label');
+const duree_ep = document.getElementById('duree_ep');
+const duree_ep_label = document.getElementById('duree_ep_label');
+const nb_tome = document.getElementById('nb_tome');
+const nb_tome_label = document.getElementById('nb_tome_label');
 
 function affiche_selon_choix(action){
-    console.log("JS2");
     for (let i=1; i<4;i++) {
- 	if (document.getElementById('film'+i).checked)
-	{
+        if (action == "Film")
+            {
+            duree.style.display="block";
+            duree_label.style.display="block";
 
-}
-}
-    if (action == "Film")
-    {
-        document.getElementById('duree').style.display="block";
-        document.getElementById('duree_label').style.display="block";
+            nb_ep.style.display="none";
+            nb_ep_label.style.display="none";
 
-        document.getElementById('nb_ep').style.display="none";
-        document.getElementById('nb_ep_label').style.display="none";
+            duree_ep.style.display="none";
+            duree_ep_label.style.display="none";
 
-        document.getElementById('duree_ep').style.display="none";
-        document.getElementById('duree_ep_label').style.display="none";
+            nb_tome.style.display="none";
+            nb_tome_label.style.display="none";
 
-        document.getElementById('nb_tome').style.display="none";
-        document.getElementById('nb_tome_label').style.display="none";
-    }
+            duree.required=true;
+            nb_ep.style.required=false;
+            duree_ep.required=false;
+            nb_tome.required=false;
+
+            film.checked=true;
+            serie.checked=false;
+            livre.checked=false;
+        }
     if (action == "Série")
-    {
-        document.getElementById('duree').style.display="none";
-        document.getElementById('duree_label').style.display="none";
+        {
+            duree.style.display="none";
+            duree_label.style.display="none";
 
-        document.getElementById('nb_ep').style.display="block";
-        document.getElementById('nb_ep_label').style.display="block";
+            nb_ep.style.display="block";
+            nb_ep_label.style.display="block";
 
-        document.getElementById('duree_ep').style.display="block";
-        document.getElementById('duree_ep_label').style.display="block";
+            duree_ep.style.display="block";
+            duree_ep_label.style.display="block";
 
-        document.getElementById('nb_tome').style.display="none";
-        document.getElementById('nb_tome_label').style.display="none";
-    }
+            nb_tome.style.display="none";
+            nb_tome_label.style.display="none";
+
+            duree.required=false;
+            nb_ep.style.required=true;
+            duree_ep.required=true;
+            nb_tome.required=false;
+
+            film.checked=false;
+            serie.checked=true;
+            livre.checked=false;
+        }
     if (action == "Livre") {
-        document.getElementById('duree').style.display="none";
-        document.getElementById('duree_label').style.display="none";
+            duree.style.display="none";
+            duree_label.style.display="none";
 
-        document.getElementById('nb_ep').style.display="none";
-        document.getElementById('nb_ep_label').style.display="none";
+            nb_ep.style.display="none";
+            nb_ep_label.style.display="none";
 
-        document.getElementById('duree_ep').style.display="none";
-        document.getElementById('duree_ep_label').style.display="none";
+            duree_ep.style.display="none";
+            duree_ep_label.style.display="none";
 
-        document.getElementById('nb_tome').style.display="block";
-        document.getElementById('nb_tome_label').style.display="block";
-    }
+            nb_tome.style.display="block";
+            nb_tome_label.style.display="block";
+
+            duree.required=false;
+            nb_ep.style.required=false;
+            duree_ep.required=false;
+            nb_tome.required=true;
+            
+            film.checked=false;
+            serie.checked=false;
+            livre.checked=true;
+        }
+    }   
 }
