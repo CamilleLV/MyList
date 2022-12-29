f9d43f8448192620f73bf144d8d39df2e304fb12
Adrien TACHER <tacheradrien@gmail.com>
2022-12-29 11:14:43 +0100

    modification css

--- view/homepage.php
+++ view/homepage.php
@@ -1,64 +1,58 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <title>MySQL</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="style_index.css">
-    </head>
-    <body>
-        <section>
-            <h1>tout les films</h1>
-            <ul class="carousel">
-                <?php
-                    //On affiche chaque film un par un
-                    foreach ($recipes as $recipe) {
-                        echo '<li>
-                                <h2>'.$recipe["title"].'</h2>
-                                <a href="index.php?action=oeuvre&id='.$recipe["id"].'"><img src='.$recipe["url_img"].' class="agrandir_oeuvre"></a>
-                            </li>';
-                    }
-                ?>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../Assets/CSS/index.css">
+    <link rel="stylesheet" type="text/css" href="../Assets/CSS/commun.css">
+    <title>Accueil | MyList</title>
+</head>
+
+<body onload="diaporama()">
+    <header>
+        <nav>
+            <a href="index.html"><img id="logoMyList" src="../Assets/Images/Logo_MyList.png" alt="Logo MyList"
+                    width="100   " height="100"></a>
+            <img id="personnage" src="../Assets/Images/Personnage.png" alt="Logo personnage" width="100" height="100">
+
+            <ul style="padding:0;">
+                <li>
+                    <h1><a href="librairie.html">Librairie</a></h1>
+                </li>
+                <li>
+                    <h1><a href="suggerer_oeuvre.html">Suggérer une oeuvre</a></h1>
+                </li>
             </ul>
-        </section>
-        <section>
-            <h1>films avec une note = à 5</h1>
-            <ul class="carousel">
-                <?php
-                    //On affiche chaque film un par un
-                    foreach ($recipes as $recipe) {
-                        if (($recipe["note_spectateurs"] === "5,0") && ($recipe["note_spectateurs"] !== "")){
-                            echo '<li>
-                                    <h2>'.$recipe["title"].'</h2>
-                                    <a href="index.php?action=oeuvre&id='.$recipe["id"].'"><img src='.$recipe["url_img"].' class="agrandir_oeuvre"></a>
-                                </li>';
-                        }
-                    }
-                ?>
-            </ul>
-        </section>
-        <section>
-            <h1> 10 films aléatoire</h1>
-            <!-- on peut avoir plusieur fois le même film-->
-            <ul class="carousel">
-                <?php
-                    //On affiche chaque film un par un
-                    $nb_film = 0;
-                    while ($nb_film !== 10) {
-                        $i = 0;
-                        $x = rand(0, count($recipes));
-                        foreach ($recipes as $recipe) {
-                            if ($i === $x){
-                                echo '<li>
-                                        <h2>'.$recipe["title"].'</h2>
-                                        <a href="index.php?action=oeuvre&id='.$recipe["id"].'"><img src='.$recipe["url_img"].' class="agrandir_oeuvre"></a>
-                                    </li>';
-                                $nb_film++;
-                            }
-                            $i++;
-                        }
-                    }
-                ?>
-            </ul>
-        </section>
-    </body>
+        </nav>
+    </header>
+
+    <h2> Bienvenue sur MyList ! </h2>
+
+    <form id="searchbox" method="get" action="search">
+        <input name="q" type="text" size="15" placeholder="Type here…">
+        <input id="button-submit" type="submit" value="Search">
+    </form>
+
+    <h3> Les Classiques : </h3>
+    <div id="classiques" class="">
+        <?php
+            //On affiche chaque film un par un
+            foreach ($recipes as $recipe) {
+                echo '<li>
+                        <a href="index.php?action=oeuvre&id='.$recipe["id"].'"><img src='.$recipe["url_img"].'></a>
+                    </li>';
+            }
+        ?>
+    </div>
+    <h3> Recommandations : </h3>
+    <div id="recommandations" class="">
+        
+    </div>
+
+    <h3> </h3>
+    <footer>
+        <h3>Nous contacter: </h3>
+    </footer>
+</body>
+
 </html>
