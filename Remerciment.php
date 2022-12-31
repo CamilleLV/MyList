6a6bcccba354692fd0924b6cd8fb0cc4bcbc7190
JIN VANESSA p2104934 <vanessa.jin@etu.univ-lyon1.fr>
2022-12-31 04:13:24 +0000

    Ajout page Remerciment.php

---
+++ Remerciment.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="FR-fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Remerciment</title>
+</head>
+<body>
+    <h1>Bienvenue vous êtes désormais inscrit(e) à MyList !</h1>
+</body>
+</html>
