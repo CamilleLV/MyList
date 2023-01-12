abf14f4c8172a7f8aa21d638ddfd7275c9fa78f8
BROUTIER CHARLENE p2100510 <charlene.broutier@etu.univ-lyon1.fr>
2023-01-12 10:36:29 +0000

    Homepage avec le slider

---
+++ MVC/homepage.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="icon" href="./Assets/Images/Logo_MyList.png" />
+    <link rel="stylesheet" type="text/css" href="Assets/CSS/index.css">
+    <link rel="stylesheet" type="text/css" href="Assets/CSS/commun.css">
+    <link rel="stylesheet" type="text/css" href="Assets/CSS/swiper-bundle.min.css">
+
+    <!-- Swiper CSS -->
+    <link rel="stylesheet" href="css/swiper-bundle.min.css" />
+
+    <title>Accueil | MyList</title>
+</head>
+
+<body onload="diaporama()"> <!-- diaporama() provenant d'index.js aka programme inutile qui affiche dans les classiques le tableau -->
+    <h2> Bienvenue sur MyList ! </h2>
+
+    <form id="searchbox" action="index.php" method="get">
+        <input name=titre type="search" size="15" placeholder="Rechercher…">
+        <input name="action" id="button-submit" type="submit" value="Rechercher">
+    </form>
+
+    <h3> Les Classiques : </h3>
+    <div id="classiques" class="">
+    </div>
+    <h3> Recommandations : </h3>
+    <div id="recommandations" class="">
+        <section>
+            <h1>Tous Les Films</h1>
+            <div class="container swiper">
+      <div class="slide-container">
+        <div class="card-wrapper swiper-wrapper">
+          <div class="card swiper-slide">
+            <div class="image-box">
+              <img src="Assets/Images/i0.jpg" alt="" />
+            </div>
+            <div class="details">
+              <div class="name-job">
+                <h3 class="name">Narnia</h3>
+              </div>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-box">
+            <img src="Assets/Images/i0.jpg" alt="" />
+            </div>
+            <div class="details">
+              <div class="name-job">
+                <h3 class="name">Narnia</h3>
+              </div>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-box">
+            <img src="Assets/Images/i0.jpg" alt="" />
+            </div>
+            <div class="details">
+              <div class="name-job">
+                <h3 class="name">Narnia</h3>
+              </div>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-box">
+            <img src="Assets/Images/i0.jpg" alt="" />
+            </div>
+            <div class="details">
+              <div class="name-job">
+                <h3 class="name">Narnia</h3>
+              </div>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-box">
+            <img src="Assets/Images/i0.jpg" alt="" />
+            </div>
+            <div class="details">
+              <div class="name-job">
+                <h3 class="name">Narnia</h3>
+              </div>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-box">
+            <img src="Assets/Images/i0.jpg" alt="" />
+            </div>
+            <div class="details">
+              <div class="name-job">
+                <h3 class="name">Narnia</h3>
+              </div>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-box">
+            <img src="Assets/Images/i0.jpg" alt="" />
+            </div>
+            <div class="details">
+              <div class="name-job">
+                <h3 class="name">Narnia</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="swiper-button-next swiper-navBtn"></div>
+      <div class="swiper-button-prev swiper-navBtn"></div>
+      <div class="swiper-pagination"></div>
+    </div>
+
+    <script src="Assets/JS/swiper-bundle.min.js"></script>
+    <script src="Assets/JS/index.js"></script>
+  </body>
+</body>
+
+</html>
