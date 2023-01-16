0995144b14c25eb9555ff663a68c165071c3baa3
BROUTIER CHARLENE p2100510 <charlene.broutier@etu.univ-lyon1.fr>
2023-01-16 14:11:35 +0000

    Changement de la page pour créer le lien avec toutespages.css

---
+++ MVC/404.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+    <link rel="stylesheet" type="text/css" href="../Assets/CSS/404.css">
+    <link rel="stylesheet" type="text/css" href="../Assets/CSS/toutespages.css">
+    <title>Erreur 404 | MyList</title>
+
+</head>
+    <body>
+        <h1> Oops! La page que vous recherchez n'existe pas! </h1>
+        <img src="https://media.tenor.com/lx2WSGRk8bcAAAAC/pulp-fiction-john-travolta.gif" id="image">
+    </body>
+</html>
