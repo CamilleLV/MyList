8fa5785320d79a9c1142de8fd5e7b34a2f3ad30c
BROUTIER CHARLENE p2100510 <charlene.broutier@etu.univ-lyon1.fr>
2023-01-16 17:04:03 +0000

    Ajout des mentions légales et du contact

--- MVC/footer.php
+++ MVC/footer.php
@@ -2,5 +2,6 @@
 
 <h3> </h3> <!-- C'est pour faire un espace visuel mais faudrait trouver une solution plus belle dans le code -->
 <footer>
-        <h3>Nous contacter: </h3>
+    <a href="View/contact.php"><h3>Nous contacter</h3></a>
+    <a href="View/mentionsLegales.html"><h3>Mentions légales</h3></a>
     </footer>
