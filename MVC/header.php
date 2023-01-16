74fbb55b27926db96899238633587ec964ef7f56
BROUTIER CHARLENE p2100510 <charlene.broutier@etu.univ-lyon1.fr>
2023-01-16 14:10:08 +0000

    Corrections sur le header

---
+++ MVC/header.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" type="text/css" href="../Assets/CSS/toutespages.css">
+
+<header>
+    <nav>
+    <a href="index.php"><img id="logoMyList" src="Assets/Images/Logo_MyList.png" alt="Logo MyList" width="120   "
+                height="100"></a>
+                
+            <a href="../registration/userPage.php"><img id="personnage" src="Assets/Images/Personnage.png"
+                    alt="Logo personnage" width="150" height="115"></a>
+        </div>
+
+        <ul style="padding:0;">
+            <li>
+                <h1><a href="index.php?action=librairie">Librairie</a></h1>
+            </li>
+            <li>
+                <h1><a href="suggerer_oeuvre.html">Suggérer une oeuvre</a></h1>
+            </li>
+            <li>
+                <h1><a href="">Modifier le profil</a></h1>
+            </li>
+        </ul>
+    </nav>
+</header>
