04ca7e9c9f88bd7ffedd2fe5bdc0a96a43de5547
BROUTIER CHARLENE p2100510 <charlene.broutier@etu.univ-lyon1.fr>
2023-01-16 14:11:53 +0000

    Changement de la page pour créer le lien avec toutespages.css

---
+++ MVC/footer.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" type="text/css" href="../Assets/CSS/toutespages.css">
+
+<h3> </h3> <!-- C'est pour faire un espace visuel mais faudrait trouver une solution plus belle dans le code -->
+<footer>
+        <h3>Nous contacter: </h3>
+    </footer>
