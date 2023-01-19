62085eaa10012a35b6ceef898e3fc416b86cfe8e
Camille Laverie <camille.laverie@etu.univ-lyon1.fr>
2023-01-19 08:35:29 +0100

    Tentative d'implémentation de suggerer Oeuvre

--- MyList/Controller/suggerer_oeuvre.php
+++ MyList/Controller/suggerer_oeuvre.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once("Model/model.php");
+//require_once("Model/suggerer_oeuvre.php");
 function sugOeuvre()
 {
-    $values = recupListUser();
+    
     require("./View/header.php");
     require("./View/suggerer_oeuvre.php");
     require("./View/footer.php");
